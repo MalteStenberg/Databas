576359a9ff4b212e606444df946132e2b205b077
MalteStenberg <112383804+MalteStenberg@users.noreply.github.com>
2023-05-05 10:24:44 +0200

    nbb
    bbijbi

---
+++ Connect_DB_to_VS/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect_DB_to_VS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //OleDbConnection conn = new OleDbConnection();
+            //conn.ConnectionString = "Provider = Mincrosoft.ace.Oledb.12.0; Data Source = infodb.accdb";
+            //conn.Open();
+            //MessageBox.Show("ok");
+
+            Form2 frmAdd = new Form2();
+            frmAdd.ShowDialog();
+
+
+
+        }
+    }
+}
---
+++ Connect_DB_to_VS/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connect_DB_to_VS
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection();
+            conn.ConnectionString = "Provider = Microsoft.ace.Oledb.12.0; Data Source = Info.accdb";
+            conn.Open();
+            //MessageBox.Show("ok");
+
+
+            OleDbCommand comm = new OleDbCommand();
+            comm.CommandText = "insert into Tabel1(FName, LName) values('Maryam', 'Rezaee')";
+            comm.Connection = conn;
+            comm.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Data successfully saved");
+        }
+    }
+}
